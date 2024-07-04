4a109a9249b96b488b4795dbfa3a9c8eb7261b4d
Nyckoly <nyckoly.kssilva@senacsp.edu.br>
2024-07-04 17:26:54 -0300

    Refatorando código e criando Get All.

--- applembretes/hello.php
+++ applembretes/hello.php
@@ -1,11 +1,16 @@
 <?php
-// servidor, bd, usuario, senha
-$db_servidor = 'localhost';
-$db_database = 'apilembretesdb';
-$db_usuario = 'Nyckoly';
-$db_pwd = '12345678';
-
-$pdo = new PDO()
 
 
+require('./../config.php');
+
+$array[]=[
+    "error"=>"",
+    "result"=> [
+
+    ]
+]
+
+require('./../return.php');
+
+exit;
 ?>
